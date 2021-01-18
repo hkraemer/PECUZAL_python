6d4bfe75e9ddb6fb1bf0f0ed3935ad5bc705b4fd
K. Hauke Kraemer <hkraemer@pik-potsdam.de>
2021-01-18 10:25:41 +0100

    update compute_documentation...

--- docs/compute_documentation_examples.py
+++ docs/compute_documentation_examples.py
@@ -14,17 +14,27 @@
 
 #' Univariate example
 import numpy as np
-data = np.genfromtxt('./data/roessler_test_series.csv')
+from scipy.integrate import odeint
 
 import matplotlib.pyplot as plt
-from pecuzal_embedding import *
+from pecuzal_embedding import pecuzal_embedding, mi
 
-y = data[:5000,1]   # bind the data
+# integrate Roessler system on standard parameters
+def roessler(x,t):
+   return [-x[1]-x[2], x[0]+.2*x[1], .2+x[2]*(x[0]-5.7)]
+
+x0 = [1., .5, 0.5] # define initial conditions
+tspan = np.arange(0., 5000.*.2, .2) # time span
+data = odeint(roessler, x0, tspan, hmax = 0.01)
+
+data = data[2500:,:]    # remove transients
+
+y = data[:,1]   # bind only y-component
 muinf, lags = mi(y)    # compute mutual information up to default maximum time lag
 
 plt.figure(figsize=(6., 8,))
 plt.subplot(2,1,1)
-plt.plot(range(len(y)),y)
+plt.plot(range(len(y[:1000])),y[:1000])
 plt.grid()
 plt.xlabel('time [in sampling units]')
 plt.title('y-component of Roessler test time series')
@@ -39,10 +49,7 @@ plt.title('Mutual information for y-component of Roessler test time series')
 plt.subplots_adjust(hspace=.3)
 plt.savefig('mi_and_timeseries_y_comp.png')
 
-
-Y_reconstruct, tau_vals, ts_vals, Ls, eps = pecuzal_embedding(y, taus = range(100), theiler = 30)
-
-from mpl_toolkits import mplot3d
+Y_reconstruct, tau_vals, ts_vals, Ls, eps = pecuzal_embedding(y, taus = range(100), theiler = 7)
 
 fig = plt.figure(figsize=(14., 8.))
 ax = plt.subplot(121, projection='3d')
@@ -52,7 +59,6 @@ ax.set_xlabel('y(t+{})'.format(tau_vals[0]))
 ax.set_ylabel('y(t+{})'.format(tau_vals[1]))
 ax.set_zlabel('y(t+{})'.format(tau_vals[2]))
 ax.set_title('PECUZAL reconstructed Roessler system')
-ax.view_init(50, 70)
 
 ax = plt.subplot(122, projection='3d')
 ax.plot(data[:5000,0], data[:5000,1], data[:5000,2], 'gray')
@@ -84,9 +90,6 @@ print(Ls)
 
 ## Multivariate example
 
-data = np.genfromtxt('./data/roessler_test_series.csv')
-data = data[:5000,:]
-
 N = len(data)
 mis = np.empty(shape=(50,3))
 for i in range(3):
@@ -99,7 +102,7 @@ ts_str = ['x','y','z']
 cnt = 0
 for i in range(0,6,2):
     plt.subplot(3,2,i+1)
-    plt.plot(range(N),data[:,cnt])
+    plt.plot(range(1000),data[:1000,cnt])
     plt.grid()
     if i == 4:
         plt.xlabel('time [in sampling units]')
@@ -116,12 +119,13 @@ for i in range(0,6,2):
 plt.subplots_adjust(hspace=.3)
 plt.savefig('mi_and_timeseries_multi.png')
 
-Y_reconstruct, tau_vals, ts_vals, Ls, eps = pecuzal_embedding(data, taus = range(100), theiler = 30)
+Y_reconstruct, tau_vals, ts_vals, Ls, eps = pecuzal_embedding(data, taus = range(100), theiler = 7)
 
 print(tau_vals)
 print(ts_vals)
 
-L_total = np.amin(Ls)
+print(Ls)
+L_total = np.sum(Ls[:-1])
 print(L_total)
 
 ts_labels = ['x','y','z']
@@ -134,7 +138,7 @@ ax.set_xlabel('{}(t+{})'.format(ts_labels[ts_vals[0]],tau_vals[0]))
 ax.set_ylabel('{}(t+{})'.format(ts_labels[ts_vals[1]],tau_vals[1]))
 ax.set_zlabel('{}(t+{})'.format(ts_labels[ts_vals[2]],tau_vals[2]))
 ax.set_title('PECUZAL reconstructed Roessler system (multivariate)')
-ax.view_init(38, -25)
+ax.view_init(-115, 30)
 
 ax = plt.subplot(122, projection='3d')
 ax.plot(data[:5000,0], data[:5000,1], data[:5000,2], 'gray')
@@ -147,8 +151,6 @@ plt.savefig('reconstruction_multi.png')
 
 
 ## Stochastic example
-
-import random
 
 def ar_process(u0, alpha, p, N):
     '''Generate `N`-sample data from an auto regressive process of order 1 with autocorrelation-parameter 
