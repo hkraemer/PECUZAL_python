532a679680b9bb192aa6e8ea534a980c1430a15c
K. Hauke Kraemer <hkraemer@pik-potsdam.de>
2021-08-26 16:14:56 +0200

    managed dependencies & test revision

--- setup.py
+++ setup.py
@@ -32,6 +32,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Development Status :: 5 - Production/Stable"],
     python_requires='>=3.7',
-    install_requires=["numpy>=1.17.2","scipy>=1.3.1","scikit-learn>=0.21.3","progress>=1.5"], 
+    install_requires=["numpy=1.17.2","scipy=1.3.1","scikit-learn=0.21.3","progress=1.5"], 
 )
 
